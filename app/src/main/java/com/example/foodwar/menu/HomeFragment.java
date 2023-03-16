d02e0f8cc415d16f6ea4d2eec2b1f8e169aa9fe3
nguyenhuubinh <binhnhhe153478@fpt.edu.vn>
2023-03-16 23:38:45 +0700

    fix menu

--- app/src/main/java/com/example/foodwar/menu/HomeFragment.java
+++ app/src/main/java/com/example/foodwar/menu/HomeFragment.java
@@ -1,29 +1,36 @@
-package com.example.foodwar.menu;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import androidx.fragment.app.Fragment;
-import com.example.foodwar.R;
-import com.example.foodwar.blogs_management.MainActivityBlogs;
-import com.example.foodwar.home_management.HomeActivity;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-public class HomeFragment extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        View view = inflater.inflate(R.layout.home_review, container, false);
-        Intent intent = new Intent(view.getContext(), HomeActivity.class);
-        startActivity(intent);
-        return view;
-
-    }
-
-
-
-}
+//package com.example.foodwar.menu;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.fragment.app.Fragment;
+//import com.example.foodwar.R;
+//import com.example.foodwar.blogs_management.MainActivityBlogs;
+//import com.example.foodwar.home_management.HomeActivity;
+//import com.google.android.material.floatingactionbutton.FloatingActionButton;
+//
+//public class HomeFragment extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_menu);
+//    }
+//
+////    @Override
+////    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        View view = inflater.inflate(R.layout.home_review, container, false);
+////        Intent intent = new Intent(view.getContext(), HomeActivity.class);
+////        startActivity(intent);
+////        return view;
+////
+////    }
+//
+//
+//
+//}
--- app/src/main/java/com/example/foodwar/menu/MainMenu.java
+++ app/src/main/java/com/example/foodwar/menu/MainMenu.java
@@ -11,10 +11,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.foodwar.R;
+import com.example.foodwar.home_management.HomeActivity;
 import com.example.foodwar.user_management.LoginActivity;
 import com.example.foodwar.user_management.LoginTabFragment;
 import com.google.android.material.navigation.NavigationView;
@@ -22,6 +26,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawerLayout;
+    WebView webView;
     TextView emailmenutv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +42,32 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
+
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_home);
         }
+
+        // web view
+        webView= findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://iamafoodblog.com/al-pastor/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
+        //
     }
+
+//    @Override
+//    public void onBackPressed(){
+//        if (webView.canGoBack()){
+//            webView.goBack();
+//        }else {
+//            super.onBackPressed();
+//        }
+//
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.nav_menu, menu);
@@ -51,7 +78,9 @@ public class MainMenu extends AppCompatActivity implements NavigationView.OnNavi
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                Intent intent= new Intent(MainMenu.this, HomeActivity.class);
+                startActivity(intent);
+                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
                 break;
             case R.id.nav_settings:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFragment()).commit();
